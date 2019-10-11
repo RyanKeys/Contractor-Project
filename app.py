10c522e9d942466e2e06bf0dc89551e9c63a8da6
Ryan Keys <r.keys1998@gmail.com>
2019-10-10 22:53:13 -0700

    Finialized App

--- app.py
+++ app.py
@@ -1,23 +1,68 @@
-from flask import Flask, render_template, url_for, request
-import requests
-from googlesearch.googlesearch import GoogleSearch
+from flask import Flask, render_template, url_for, request, redirect
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-listing_params = {
-    "item": "Guitar",
-    "brand": "Gibson"
-}
+client = MongoClient()
+db = client.Website
+guitars = db.guitars
 
-google_search = GoogleSearch().search(listing_params)
 
 app = Flask(__name__)
 
+'''guitars = [
+    {'name': 'Gibson', "type": "Les Paul"},
+    {'name': 'Fender', "type": "Strat"}
+]'''
+
 
 @app.route('/')
-def show_app():
-    return render_template('home.html')
+def guitars_index():
+    return render_template('guitars_index.html', guitars=guitars.find())
 
 
-@app.route('/new')
-def new_listings():
+@app.route('/guitars', methods=['POST'])
+def submit_listing():
+    guitar = {
+        'name': request.form.get('name'),
+        'description': request.form.get('description')
+    }
+    guitar_id = guitars.insert_one(guitar).inserted_id
+    return redirect(url_for('guitars_show', guitar_id=guitar_id))
 
-    return render_template('new_listings.html', google_search=google_search)
+
+@app.route('/guitars/new')
+def guitars_new():
+    return render_template("guitars_new.html")
+
+
+@app.route('/guitars/<guitar_id>')
+def guitars_show(guitar_id):
+    guitar = guitars.find_one({'_id': ObjectId(guitar_id)})
+    return render_template("guitars_show.html", guitar=guitar)
+
+
+@app.route('/guitars/<guitar_id>/edit')
+def guitars_edit(guitar_id):
+    """Show the edit form for a guitar."""
+    guitar = guitars.find_one({'_id': ObjectId(guitar_id)})
+    return render_template('guitars_edit.html', guitar=guitar)
+
+
+@app.route('/guitars/<guitar_id>', methods=['POST'])
+def guitars_update(guitar_id):
+    """Submit an edited guitar."""
+    updated_guitar = {
+        'name': request.form.get('name'),
+        'description': request.form.get('description'),
+    }
+    guitars.update_one(
+        {'_id': ObjectId(guitar_id)},
+        {'$set': updated_guitar})
+    return redirect(url_for('guitars_show', guitar_id=guitar_id))
+
+
+@app.route('/guitars/<guitar_id>/delete', methods=['POST'])
+def guitars_delete(guitar_id):
+    """Delete one guitar."""
+    guitars.delete_one({'_id': ObjectId(guitar_id)})
+    return redirect(url_for('guitars_index'))
